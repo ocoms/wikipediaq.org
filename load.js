85895209860c35044fc8de6d9a80ae3e6755bd09
ocoms <96325433+ocoms@users.noreply.github.com>
2021-12-22 17:19:36 -0500

    Create load.js

---
+++ load.js
@@ -0,0 +1,10 @@
+var title = "Wikipedia.org";
+var targetdomain = "wikipedia.org";
+var domain = "wikipediaq.org";
+var hrefPage = "https://www.wikipedia.org/";
+
+document.getElementById("targetdomain").innerHTML = "Were you trying to reach: <b>" + targetdomain + "</b>?";
+document.getElementById("domain").innerHTML = "Continue to " + targetdomain;
+document.getElementById("continueto").href = hrefPage;
+
+document.getElementById("title").innerHTML = title;
